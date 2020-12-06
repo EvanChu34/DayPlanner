1fe980de61f18eafc86f96213f6f31d50aad7bf8
Evan Chu <echu34@gmail.com>
2020-12-06 18:25:46 -0500

    Added Script.js and removed from index

---
+++ script.js
@@ -0,0 +1,20 @@
+// Timeblocks go here -->
+DateTime.local().setZone('America/New_York').minus({ weeks: 1 }).endOf('day').toISO();
+      
+var daySlot = [
+
+];
+
+
+function getCurrentDate(){
+    
+
+};
+
+// standard work hours 9-5 --> 
+
+// color coded work  --> 
+
+// type and save button --> 
+
+// refresh but it saves JSON Local --> 
