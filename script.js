328e9115623144ea3e261e0bf763a6e982620b31
Evan Chu <echu34@gmail.com>
2020-12-06 22:57:40 -0500

    Added more Var. will work on functions tomorrow

--- script.js
+++ script.js
@@ -5,57 +5,80 @@ var daySlot = [
     {
         hourSlotID = "1",
         timeID= "9",
-        
+        plannedEvent="",
     },
     {
         hourSlotID = "2",
         timeID= "10",
-
+        plannedEvent="",
     },
     {
-        hourSlotID = 3,
+        hourSlotID = "3",
         timeID= "11",
-
+        plannedEvent="",
     },
     {
         hourSlotID = "4",
         timeID= "12",
-
+        plannedEvent="",
     },
     {
         hourSlotID = "5",
         timeID= "1",
+        plannedEvent="",
     },
     {
         hourSlotID = "6",
         timeID= "2",
+        plannedEvent="",
     },
     {
         hourSlotID= "7",
         timeID= "3",
+        plannedEvent="",
     },
     {
         hourSlotID = "8",
         timeID= "4",
+        plannedEvent="",
     },
     {
         hourSlotID = "9",
         timeID= "5",
+        plannedEvent="",
     }
 ];
 
-DateTime.local().setZone('America/New_York').minus({ weeks: 1 }).endOf('day').toISO();
+//DateTime.local().setZone('America/New_York').minus({ weeks: 1 }).endOf('day').toISO();
 
+
+// FUNCTIONS IN J QUERY ADJUST THEM
 function getCurrentDate(){
 
-    var currentDate = DateTime.local().toLocaleString(DateTime.DATETIME_SHORT);
+    var currentDate = DateTime.local().toLocaleString(DateTime.DATETIME_SHORT); // unsure this will work. will be testing tomorrow
+
     
 }
 
-// standard work hours 9-5 --> 
 
-// color coded work  --> 
 
 // type and save button --> 
 
-// refresh but it saves JSON Local --> 
+}
+
+function displayEvent(){
+
+}
+
+function run(){
+
+
+}
+
+
+
+// tomorrow work on functions and also connect the Luxon
+// color coded work  --> 
+// look on old code for the save button
+
+
